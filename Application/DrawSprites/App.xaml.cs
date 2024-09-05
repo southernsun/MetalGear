33c61b0dd358a034faf788dc101de3263e5b19ae
southernsun <southernsun@users.noreply.github.com>
2024-09-05 16:44:20 +0200

    Adding DrawSprites application.

---
+++ Application/DrawSprites/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DrawSprites
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
