e63882e27270f5d0a0c45521a9a643b943b80d9b
southernsun <southernsun@users.noreply.github.com>
2024-09-10 20:11:57 +0200

    Added Resources and Snake Sprite attributes.

---
+++ Application/DrawSprites/PlayerSprite/SnakeSpriteAttributes.cs
@@ -0,0 +1,13 @@
+﻿namespace DrawSprites.PlayerSprite;
+
+public class SnakeSpriteAttributes
+{
+    public byte NumberOfSprites { get; set; }
+    public SpriteAttribute[] Attributes { get; set; }
+
+    public SnakeSpriteAttributes(byte numberOfSprites, SpriteAttribute[] attributes)
+    {
+        NumberOfSprites = numberOfSprites;
+        Attributes = attributes;
+    }
+}
---
+++ Application/DrawSprites/PlayerSprite/SnakeSpriteAttributesEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawSprites
+{
+    public enum SnakeSpriteAttributes
+    {
+        SnakeAttrShare,
+        SnakeAttrWater,
+        SnakeAttrShareDying,  // Dying (!?) Probably it had its own attributes
+        SnakeAttrBox,
+        WaterShadowAttr,
+        SnakeAttrParach,
+        SnakeAttrPunchR,
+        SnakeAttrDead,
+        SnakeAttrBox2,
+        SnakeAttrWaterW
+    }
+}
